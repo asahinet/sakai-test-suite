05f4cd2746a32685cacbfdfba683b33dd7c88ca5
John Bush <jbush@rsmart.com>
2013-09-03 16:03:15 -0700

    add screenshot trait

---
+++ src/test/scala/com/anisakai/test/cucumber/stepdefs/ScreenShotOnFailure.scala
@@ -0,0 +1,32 @@
+package com.anisakai.test.cucumber.stepdefs
+
+import cucumber.api.java.After
+import cucumber.api.Scenario
+import org.openqa.selenium.{OutputType, TakesScreenshot, WebDriver}
+import com.anisakai.test.Config
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jbush
+ * Date: 9/3/13
+ * Time: 3:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+trait ScreenShotOnFailure {
+  @After
+  def tearDown(result: Scenario) {
+    if (result.isFailed) {
+      if (webDriver.isInstanceOf[TakesScreenshot]) {
+        val screenshot = webDriver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.BYTES);
+        try {
+          result.embed(screenshot, "image/png")
+        } catch {
+          case e: Exception => e.printStackTrace()
+        }
+      }
+    }
+  }
+
+  val webDriver: WebDriver = Config.webDriver
+
+}
