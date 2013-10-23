72941c28883fb22dea0d0e85aa3e2a5a426509c1
gareth <gleonard@anisakai.net>
2013-10-23 14:24:55 -0700

    turnitin & gradebook

--- src/test/resources/com/anisakai/test/cucumber/assignment_turnitin.feature
+++ src/test/resources/com/anisakai/test/cucumber/assignment_turnitin.feature
@@ -1,26 +1,21 @@
+@assignment @turnitin
 Feature: Creating an assignment with the Third Party Tool Turn It In enabled
 
   As an Instructor I want to be able to create an assignment with Turn It In enabled
   so that students can receive feedback from Turn it in on their submitted assignments
 
-
-
   Scenario: I create a Turn It In assignment incorrectly
-    Given I'm logged in as 'instructor'
-    And I create a 'course'
-    When I add an 'assignment'
-    And I enable turn it in
-    But I don't choose Single upload file
-    Then I should get a warning message
+    Given I'm logged in as an 'instructor' on a Turn It In instance
+    When I add a Turn It In assignment 'incorrectly'
+    Then The status of the assignment should be 'Not Open'
 
   Scenario: I create a Turn It In assignment correctly
-    Given I'm logged in as an Instructor
-    And I create a 'course'
-    And I add a Turn It In assignment
-    Then The status of the assignment should be open
+    Given I'm logged in as an 'instructor' on a Turn It In instance
+    And I add a Turn It In assignment 'correctly'
+    Then The status of the assignment should be 'Open'
 
   Scenario: Submitting a Turn It In assignment as a student
     Given A course exists with a Turn It In assignment
-    And I'm logged in as a student
-    When I submit an assingment
-    Then The status should be submitted
+    And I'm logged in as a 'student' on a Turn It In instance
+    When I submit a Turn It In assignment
+    Then The status of the assignment should be 'Submitted'
---
+++ src/test/scala/com/anisakai/test/cucumber/stepdefs/TurnItInTest.scala
@@ -0,0 +1,82 @@
+package com.anisakai.test.cucumber.stepdefs
+
+import cucumber.api.scala.{EN, ScalaDsl}
+import com.anisakai.test.pageobjects.{TurnItIn, AssignmentTool, Portal}
+import junit.framework.Assert._
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gareth
+ * Date: 10/9/13
+ * Time: 1:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class TurnItInTest extends ScalaDsl with EN with TearDown {
+
+  var assignmentTitle: String = ""
+  var eid : String = ""
+  var password: String = ""
+
+  def login(role: String) {
+    Portal.navigateToPage("https://providence-test.rsmart.com/")
+    Portal.logout()
+
+    if (role.equalsIgnoreCase("admin")) {
+      eid = "rsmart_support"
+      password = "r5m@rt!"
+    } else if (role.equalsIgnoreCase("instructor")) {
+      eid = "instructorx"
+      password = "password"
+    } else if (role.equalsIgnoreCase("student")) {
+      eid = "studentx"
+      password = "password"
+    }
+    Portal.enterEid(eid)
+    Portal.enterPassword(password)
+    Portal.login()
+  }
+
+  Given("""^I'm logged in as an? '(.+)' on a Turn It In instance$"""){ (role : String) =>
+    login(role)
+  }
+
+  When("""^I add a Turn It In assignment '(.+)'$"""){ (correct: String) =>
+    Portal.gotoSite("TurnItIn Test 01")
+    Portal.gotoTool("Assignments")
+    AssignmentTool.gotoAdd()
+    if (correct.equalsIgnoreCase("correctly")) {
+      assignmentTitle = AssignmentTool.assignment(true, true)
+    } else {
+      assignmentTitle = AssignmentTool.assignment(true, false)
+    }
+  }
+
+  Then("""^I should get a warning message$"""){ () =>
+    assertTrue(TurnItIn.warningMessage())
+  }
+
+  Then("""^The status of the assignment should be '(.+)'$"""){ (status : String) =>
+    assertTrue(TurnItIn.assignmentStatus(status, assignmentTitle))
+  }
+
+  Given("""^A course exists with a Turn It In assignment$"""){ () =>
+    login("instructor")
+    Portal.gotoSite("TurnItIn Test 01")
+    Portal.gotoTool("Assignments")
+    assertTrue(TurnItIn.turnItInExists(assignmentTitle))
+  }
+
+  When("""^I submit a Turn It In assignment$""") { () =>
+    Portal.gotoSite("TurnItIn Test 01")
+    Portal.gotoTool("Assignments")
+    AssignmentTool.openAssignment(assignmentTitle)
+    TurnItIn.uploadFile()
+  }
+
+
+
+
+
+
+}
